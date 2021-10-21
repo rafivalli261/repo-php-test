8e5f4e42b74e78521c2f207b3ec936395d7c8af3
rafivalli261 <valliansyah@gmail.com>
2021-10-21 22:54:40 +0700

    Bingung episode mysql dan php

---
+++ form.php
@@ -0,0 +1,34 @@
+<?php
+    //koneksi database
+mysqli_connect("localhost","root","","phpdasar",);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Form</title>
+</head>
+
+<body>
+    <h1>Daftar Mahasiswa</h1>
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No.</th>
+            <th>Aksi</th>
+            <th>Gambar</th>
+            <th>NRP</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Jurusan</th>
+        </tr>
+        <tr>
+            <a href="#">ubah</a>
+            <a href="#">hapus</a>
+        </tr>
+    </table>
+</body>
+
+</html>
--- mulai.php
+++ mulai.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Belajar PHP</title>
 </head>
-<body style="width: 400px;font-size: 20px;font-family: Helvetica, sans-serif; margin: 0 auto;">
+
+<body style="width: 400px; font-size: 20px; font-family: 'Source Code Pro', sans-serif;  margin: 0 auto;">
     <?php
         $hello = "<h1>Apakah saya sudah berhasil membuat Hello World ?</h1>";
         echo " $hello Oh rupanya saya berhasil, Alhamdulillah<br>";
@@ -19,6 +21,10 @@
                 echo "<p style='background-color: salmon; padding : 20px'>Wuah, Angka sekarang adalah $i </p>";
             }
         }
+        $hello = "Apakah itu kamu?";
+        echo $hello;
+
     ?>
 </body>
+
 </html>
